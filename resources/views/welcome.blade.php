80f93171da278ec00876ec8123117049b495cc79
JeffW <swiftwinning@gmail.com>
2018-04-15 17:24:32 -0400

    Set up .env file, added debugbar package, and changed welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    WindowPanes
                 </div>
 
                 <div class="links">

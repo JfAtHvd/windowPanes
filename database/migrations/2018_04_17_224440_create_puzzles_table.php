18ef4179713bce6a1feb7ac7a76cf188ce99ec27
JeffW <swiftwinning@gmail.com>
2018-04-17 22:46:23 -0400

    Set local .env file to get ready for database, created migration for 'puzzles' table, created Puzzle Model/Class

---
+++ database/migrations/2018_04_17_224440_create_puzzles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePuzzlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('puzzles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            
+            $table->integer('level');
+            $table->integer('solve_time');
+            $table->integer('total_solve_time');
+            $table->text('pattern_json');
+            $table->text('level_json');
+            $table->integer('player_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('puzzles');
+    }
+}
